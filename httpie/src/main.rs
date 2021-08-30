fe88309b1341d598b693a1424d9d1c1d44f244fc
DaviRain-Su <davirain.yin@gamil.com>
2021-08-31 01:19:37 +0800

    update ...

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,7 +1,9 @@
-use std::str::FromStr;
+use std::{collections::HashMap, hash, str::FromStr};
 
 use anyhow::{anyhow, Result};
-use reqwest::Url;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{Client, Response, Url, header};
 use structopt::StructOpt;
 
 // get 子命令
@@ -87,7 +89,83 @@ pub struct App {
     pub subcommand: SubCommand,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opt = App::from_args();
     println!("{:?}", opt);
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-POWERD-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+
+    // let client = Client::new();
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    let result = match opt.subcommand {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+    
+    Ok(result)
+
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+    // println!("{:?}",resp.text().await?);
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!("");
+}
+
+/// 打印服务器返回的HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于"application/json" pretty print
+        Some(v) if v == mime::APPLICATION_JSON  => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan());
+        }
+        _ => println!("{}", body),
+    }
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, body.as_str());
+
+    Ok(())
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime>  {
+    resp
+        .headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
 }
